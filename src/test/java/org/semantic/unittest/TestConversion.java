42fabe4b9e3da1dd87f1e4e4980144c612c3af75
Lenovo <Lenovo@Lenovo_ip_510>
2018-08-19 18:05:16 +0300

    fix import

--- src/test/java/org/semantic/unittest/TestConversion.java
+++ src/test/java/org/semantic/unittest/TestConversion.java
@@ -25,7 +25,6 @@ public class TestConversion {
 			String message;
 
 			org.apache.jena.rdf.model.Model model = ModelFactory.createDefaultModel();
-			long startTime = System.currentTimeMillis();
 			HL7Converter hl7Segments = new HL7Converter(model);
 	
 			message = "MSH|^~\\&|ADT1|MCM|LABADT|MCM|198808181126|SECURITY|ADT^A01|MSG00001-|P|2.5.1\r\n"
